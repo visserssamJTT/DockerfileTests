7c3dd16c70d5e22b8ae7989f4ac13ee4739a7296
visserssamJTT <34665258+visserssamJTT@users.noreply.github.com>
2018-03-07 11:50:39 -0800

    Update postgresStartUp.sh

--- postgresStartUp.sh
+++ postgresStartUp.sh
@@ -1,4 +1,6 @@
-echo "sarting";
+#remove the container so that it can be started with out error
 docker rm Iconoclastic_Logar;
+#starting the container on port 5432 a volume with the folder containing the databse folder 
 docker run -i -t -p 5432:5432 -d -v Eternal_Crusade:/var/lib/postgresql/ --name Iconoclastic_Logar dummyapp/rubytest;
+#listing the running containers to make sure the container started
 docker ps;
