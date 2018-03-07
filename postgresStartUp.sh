64d41751c8b592a5c2e9ed91ba2fa63e56333463
visserssamJTT <34665258+visserssamJTT@users.noreply.github.com>
2018-03-07 09:55:53 -0800

    Update postgresStartUp.sh

--- postgresStartUp.sh
+++ postgresStartUp.sh
@@ -1,4 +1,4 @@
 echo "sarting";
 docker rm Iconoclastic_Logar;
-docker run -it -p 5432:5432 -d -v Eternal_Crusade:/var/lib/postgresql --name Iconoclastic_Logar;
-dicker ps;
+docker run -i -t -p 5432:5432 -d -v Eternal_Crusade:/var/lib/postgresql/ --name Iconoclastic_Logar dummyapp/rubytest;
+docker ps;
